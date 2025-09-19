38e9ac23d3bdd884ae76a78577236ab20aee90b7
Damaso Sanoja <damasosanoja@gmail.com>
2025-09-19 09:44:16 -0400

    feat: Intentionally break the greeting function for tutorial

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def get_greeting(name):
     """Generates a greeting string for the given name."""
-    return f'Hi, {name}'
+    return f'Hello, {name}'
 
 
 # Press the green button in the gutter to run the script.
